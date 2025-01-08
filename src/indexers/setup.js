08d4e484e5b2fe2c47a9045daaa7da8c1336a15c
Edgar Ochieng <82944501+49ochieng@users.noreply.github.com>
2025-01-08 13:53:16 -0600

    Update setup.js

--- src/indexers/setup.js
+++ src/indexers/setup.js
@@ -17,7 +17,7 @@ process.env.SECRET_AZURE_OPENAI_API_KEY = azureOpenAIKey;
  *  Main function that creates the index and upserts the documents.
  */
 async function main() {
-    const index = "my-document";
+    const index = "my-documents";
 
     if (
         !process.env.AZURE_SEARCH_ENDPOINT ||
